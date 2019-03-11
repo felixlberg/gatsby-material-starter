45008d09b038eba42f40d7454431339ae37c88a7
Felix <felix.lberg@gmail.com>
2019-03-11 03:34:08 +0100

    added html.jsx

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -39,7 +39,7 @@ const config = {
       iconClassName: "fa fa-envelope"
     }
   ],
-  copyright: "Copyright © 2019. Material User" // Copyright string for the footer of the website and RSS feed.
+  copyright: "Copyright © 2019. underground.consulting" // Copyright string for the footer of the website and RSS feed.
 };
 
 // Validate
