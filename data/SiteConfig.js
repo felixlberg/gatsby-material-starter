f901f6ba3df0f06ff751198859c972477e6498bb
Felix <felix.lberg@gmail.com>
2019-07-04 00:22:09 +0200

    set right url

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,7 +3,7 @@ const config = {
   siteTitleShort: "MB", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "Material Design Blog", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: " https://felixlberg.github.io", // Domain of your website without pathPrefix.
+  siteUrl: "https://felixlberg.github.io", // Domain of your website without pathPrefix.
   pathPrefix: "/gatsby-material-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
   siteDescription: "A GatsbyJS stater with Material design in mind.", // Website description used for RSS feeds/meta description tag.
